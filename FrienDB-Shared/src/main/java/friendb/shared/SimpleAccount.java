b7486c2c64adf888b1b1983d395c5da68c0b4ee6
Nathan Wong <nathan.cw@live.com>
2015-04-27 18:13:54 -0400

    Creation of Simple Classes
    Created simple classes of all the entities so far

---
+++ FrienDB-Shared/src/main/java/friendb/shared/SimpleAccount.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package friendb.shared;
+
+/**
+ *
+ * @author nathanwong
+ */
+public class SimpleAccount {
+    
+    private int customerID;
+    private int  accountNum;
+    private String creationDate;
+    private String creditCard;
+    
+}
---
+++ FrienDB-Shared/src/main/java/friendb/shared/SimpleCircleMembership.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package friendb.shared;
+
+/**
+ *
+ * @author nathanwong
+ */
+public class SimpleCircleMembership {
+    
+    private int circleID;
+    private int customerID;
+    
+}
