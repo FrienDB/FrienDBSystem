6cfa0944388b44ba9a0e02791755c98a6fabd525
gubyy <evanguby@adm-129-49-224-178.wi-fi.stonybrook.edu>
2015-04-29 14:59:26 -0400

    Revert "Revert 780e2f5..5be28a5"
    This reverts commit 3f2714af0954fd055e5acba4eec00acbfe0c30e7.

---
+++ FrienDB-Shared/src/main/java/friendb/shared/SimpleAdvertisement.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package friendb.shared;
+
+/**
+ *
+ * @author nathanwong
+ */
+public class SimpleAdvertisement {
+
+    private int adID;
+    private int employeeID;
+    private String adType;
+    private String postDate;
+    private String company;
+    private String item;
+    private String content;
+    private double price;
+    private int numOfUnits;
+
+}
