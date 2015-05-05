a9e3f2d7d4c826189c32b3aa958ead7950039021
Nathan Wong <nathan.cw@live.com>
2015-05-05 19:18:00 -0400

    handleMakePost
    not done yet

--- FrienDB-Client/src/friendb/client/view/NewPostPageController.java
+++ FrienDB-Client/src/friendb/client/view/NewPostPageController.java
@@ -8,12 +8,20 @@ package friendb.client.view;
 import friendb.client.main.ControlledScreen;
 import friendb.client.main.FrienDBClient;
 import friendb.client.main.ScreensController;
+import friendb.client.session.CustomerSession;
+import friendb.client.web.ServerAccessPoint;
+import friendb.client.web.ServerResources;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import friendb.shared.SimplePost;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.ws.rs.core.Response;
 
 /**
  * FXML Controller class
@@ -26,6 +34,9 @@ public class NewPostPageController implements Initializable, ControlledScreen {
     
     @FXML
     private Label circleName;
+    
+    private final ServerAccessPoint addNewCircle =
+            new ServerAccessPoint(ServerResources.ADD_POST_URL);
 
     /**
      * Initializes the controller class.
@@ -42,6 +53,21 @@ public class NewPostPageController implements Initializable, ControlledScreen {
 
     @FXML
     private void handleMakePost(ActionEvent event) {
+        CustomerSession cs = (CustomerSession)myController.getSession();
+        SimplePost newPost = new SimplePost();
+        
+        newPost.pageID = cs.getPageID();
+//        newPost.content = 
+//        newPost.authorID = Integer.parseInt( (cs.getCustomerAccount().CustomerID) );
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        String value = dateFormat.format(date);
+        String change = value.replaceAll("/", "-");
+        newPost.datePosted = change;
+
+        Response rsp = addNewCircle.request(newPost);
+        
         myController.setScreen(FrienDBClient.YourCirclePageID);
     }
 
