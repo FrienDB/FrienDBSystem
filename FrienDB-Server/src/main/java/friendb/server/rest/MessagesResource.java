aefe9d473091646134918d18ee7a75624c19957b
Nathan Wong <nathan.cw@live.com>
2015-05-07 02:09:40 -0400

    fix tenbillion

--- FrienDB-Server/src/main/java/friendb/server/rest/MessagesResource.java
+++ FrienDB-Server/src/main/java/friendb/server/rest/MessagesResource.java
@@ -11,8 +11,10 @@ import friendb.shared.SimpleMessages;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
@@ -23,6 +25,8 @@ import javax.ws.rs.core.UriInfo;
  *
  * @author nathanwong
  */
+@Path("message") //the url at which this web service's resources are accessed
+@RequestScoped
 public class MessagesResource {
     
     @Context
